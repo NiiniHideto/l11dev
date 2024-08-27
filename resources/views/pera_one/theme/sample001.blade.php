4141f2fa138d271dc6b7277d5a3f72f5d5c45b88
niimi <hideto.niimi@1plus.co.jp>
2024-08-27 13:07:26 +0900

    テーマ修正

--- resources/views/pera_one/theme/sample001.blade.php
+++ resources/views/pera_one/theme/sample001.blade.php
@@ -4,41 +4,46 @@
 @section('content')
 <section class="text-gray-600 body-font relative">
 
-    <!-- 背景画像 画像A -->
+    <!-- 画像A 背景画像 -->
     <header style="background-position: center; background-size: cover; background-attachment: fixed; background-image:url( {{asset('storage/' . $pera_one->pic_a)}} );">
 
     @if (isset($pera_one))
         <div class="container py-5 px-10 mx-auto">
 
+            <!-- 文章A タイトル風 -->
             <ul class=" mx-auto w-1/2 font-medium text-gray-900 bg-white/50 rounded-lg border border-gray-200">
                 <li class="flex justify-center py-4 px-5 w-full rounded-t-lg border-b last:border-b-0 border-gray-200">
-                    <p>{{ $pera_one->str_a }}</p>
+                    <p>{!! nl2br(htmlspecialchars($pera_one->str_a)) !!}</p>
                 </li>
             </ul>
             <br>
 
+            <!-- 画像B 文字列合成 -->
             <img src="{{asset('storage/' . $pera_one->pic_b . '_test')}}" style="display: block; margin: auto;" />
             <br>
             <br>
 
-
+            <!-- 画像B 元画像 -->
             <img src="{{asset('storage/' . $pera_one->pic_b)}}" style="display: block; margin: auto;" />
             <br>
 
+            <!-- 文章B -->
             <ul class="font-medium text-gray-900 bg-white/80 rounded-lg border border-gray-200">
                 <li class="py-4 px-5 w-full rounded-t-lg border-b last:border-b-0 border-gray-200">
-                    <p>{{ $pera_one->str_b }}</p>
+                    <p>{!! nl2br(htmlspecialchars($pera_one->str_b)) !!}</p>
                 </li>
             </ul>
             <br>
             <br>
 
+            <!-- 画像C -->
             <img src="{{asset('storage/' . $pera_one->pic_c)}}" style="display: block; margin: auto;" />
             <br>
 
+            <!-- 文章C -->
             <ul class="font-medium text-gray-900 bg-white/80 rounded-lg border border-gray-200">
                 <li class="py-4 px-5 w-full rounded-t-lg border-b last:border-b-0 border-gray-200">
-                    <p>{{ $pera_one->str_c }}</p>
+                    <p>{!! nl2br(htmlspecialchars($pera_one->str_c)) !!}</p>
                 </li>
             </ul>
 
