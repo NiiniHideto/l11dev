b0928cf49a6ef4006b36c21f8278444eca4d63ea
niimi <hideto.niimi@1plus.co.jp>
2024-09-26 12:14:28 +0900

    alpine.js で診断アプリ風

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,125 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
+// Alpine.start();
+
+// Alpine.start() の前にリスナーを実装
+// 診断
+document.addEventListener("alpine:init", () => {
+    Alpine.data("diagnosis", () => ({
+        step: "start",
+        answers: [],
+        result: "",
+
+        startDiagnosis() {
+            this.step = "q1";
+        },
+
+        restartDiagnosis() {
+            this.answers = [];
+            this.handleStep("start");
+        },
+
+        answerQuestion(answer) {
+            this.answers.push(answer);
+
+            if (this.step === "q1") {
+                this.handleStep("q2");
+            } else if (this.step === "q2") {
+                this.handleStep("q3");
+            } else if (this.step === "q3") {
+                this.showResult();
+            }
+        },
+
+        handleStep(toStep) {
+            this.step = toStep;
+        },
+
+        previousStep() {
+            if (this.answers.length > 0) {
+                this.answers.pop();
+            }
+
+            if (this.step === "q2") {
+                this.handleStep("q1");
+            } else if (this.step === "q3") {
+                this.handleStep("q2");
+            } else if (this.step === "q1") {
+                this.handleStep("start");
+            }
+        },
+
+        showResult() {
+            this.step = "processing";
+            setTimeout(() => {
+                this.result = this.calculateResult();
+                this.handleStep("result");
+            }, 1000);
+        },
+
+        calculateResult() {
+            const imgPath =
+            "https://www.evoworx.co.jp/wp2024/wp-content/uploads/2023/04/engunner_naganoma-1.png";
+            const pattern = this.answers.join("→");
+            switch (pattern) {
+                case "A→A→A":
+                    return {
+                    type: "活発元気系な",
+                    name: "naganoma",
+                    img: imgPath
+                    };
+                case "A→B→B":
+                    return {
+                    type: "現実主義者",
+                    name: "naganoma",
+                    img: imgPath
+                    };
+                case "A→A→B":
+                    return {
+                    type: "自分大好き",
+                    name: "naganoma",
+                    img: imgPath
+                    };
+                case "A→B→A":
+                    return {
+                    type: "隠れ情熱系",
+                    name: "naganoma",
+                    img: imgPath
+                    };
+                case "B→B→B":
+                    return {
+                    type: "オカルト大好き",
+                    name: "naganoma",
+                    img: imgPath
+                    };
+                case "B→A→A":
+                    return {
+                    type: "純情少年系",
+                    name: "naganoma",
+                    img: imgPath
+                    };
+                case "B→A→B":
+                    return {
+                    type: "野心高めな",
+                    name: "naganoma",
+                    img: imgPath
+                    };
+                case "B→B→A":
+                    return {
+                    type: "犬系彼氏",
+                    name: "naganoma",
+                    img: imgPath
+                    };
+                default:
+                    return {
+                    type: "その他",
+                    name: "診断結果なし",
+                    img: imgPath
+                    };
+            }
+        }
+    }));
+});
+
 Alpine.start();
--- resources/views/pera_one/theme/sample004.blade.php
+++ resources/views/pera_one/theme/sample004.blade.php
@@ -2,6 +2,173 @@
 
     <!-- alpine.js -->
 
+
+    <!-- 診断 -->
+    <div x-data='diagnosis' class='diagnosis-container'>
+        <div class='diagnosis-start' x-cloak x-show="step === 'start'" 
+            x-transition:enter='transitionEnter' x-transition:enter-start='enterStart' x-transition:enter-end='enterEnd' 
+            x-transition:leave='transitionLeave' x-transition:leave-start='leaveStart' x-transition:leave-end='leaveEnd'>
+            <h1 class='start-title'>診断</h1>
+            <button type='button' class='start-btn' @click="handleStep('q1')">
+                START
+            </button>
+        </div>
+        <div class='diagnosis-question' x-cloak x-show="step === 'q1'" 
+            x-transition:enter='transitionEnter' x-transition:enter-start='enterStart' x-transition:enter-end='enterEnd' 
+            x-transition:leave='transitionLeave' x-transition:leave-start='leaveStart' x-transition:leave-end='leaveEnd'>
+            <h2 class='question-title'>パートナーにするなら？</h2>
+            <div class='question-answer'>
+            <button type='button' @click="answerQuestion('A')">
+                <span>年上派</span>
+            </button>
+            <button type='button' @click="answerQuestion('B')">
+                <span>年下派</span>
+            </button>
+            </div>
+            <button class='question-back' type='button' @click='previousStep()'>
+                スタート画面に戻る
+            </button>
+        </div>
+
+        <div class='diagnosis-question' x-cloak x-show="step === 'q2'" 
+            x-transition:enter='transitionEnter' x-transition:enter-start='enterStart' x-transition:enter-end='enterEnd' 
+            x-transition:leave='transitionLeave' x-transition:leave-start='leaveStart' x-transition:leave-end='leaveEnd'>
+            <h2 class='question-title'>休日は</h2>
+            <div class='question-answer'>
+            <button type='button' @click="answerQuestion('A')">
+                <span>外でアクティブに<br />リフレッシュ</span>
+            </button>
+            <button type='button' @click="answerQuestion('B')">
+                <span>屋内で<br />まったり過ごす</span>
+            </button>
+            </div>
+            <button class='question-back' type='button' @click='previousStep()'>
+                1つ前に戻る
+            </button>
+        </div>
+
+        <div class='diagnosis-question' x-cloak x-show="step === 'q3'" 
+            x-transition:enter='transitionEnter' x-transition:enter-start='enterStart' x-transition:enter-end='enterEnd' 
+            x-transition:leave='transitionLeave' x-transition:leave-start='leaveStart' x-transition:leave-end='leaveEnd'>
+            <h2 class='question-title'>人の運命は</h2>
+            <div class='question-answer'>
+            <button type='button' @click="answerQuestion('A')">
+                <span>努力次第で<br />切り開ける</span>
+            </button>
+            <button type='button' @click="answerQuestion('B')">
+                <span>変えられない</span>
+            </button>
+            </div>
+            <button class='question-back' type='button' @click='previousStep()'>
+                1つ前に戻る
+            </button>
+        </div>
+
+        <div class='diagnosis-processing' x-cloak x-show="step === 'processing'" 
+            x-transition:enter='transitionEnter' x-transition:enter-start='enterStart' x-transition:enter-end='enterEnd' 
+            x-transition:leave='transitionLeave' x-transition:leave-start='leaveStart' x-transition:leave-end='leaveEnd'>
+            <h2>診断中...</h2>
+        </div>
+
+        <div class='diagnosis-result' x-cloak x-show="step === 'result'" 
+            x-transition:enter='transitionEnter' x-transition:enter-start='enterStart' x-transition:enter-end='enterEnd' 
+            x-transition:leave='transitionLeave' x-transition:leave-start='leaveStart' x-transition:leave-end='leaveEnd'>
+            <h2 class='result-title'>診断結果🎉</h2>
+            <p class='result-desc'>あなたの運命の相手は...</p>
+            <figure class='result-thumb'>
+            <img :src='result.img' alt='' />
+            </figure>
+            <p class='result-caption'>
+            <span class='caption-type' x-text='result.type'></span>
+            <span class='caption-name'>
+                <span x-text='result.name'></span>さん
+            </span>
+            </p>
+            <button class='result-back' @click='restartDiagnosis()'>
+                スタート画面に戻る
+            </button>
+        </div>
+    </div>
+    <br>
+
+
+    <!-- 入力された値を参照 -->
+    <div x-data="{ inputedText: '' }">
+        <input x-model="inputedText" type="text" placeholder="文字を入力" />
+        <p>入力された値：<span x-text="inputedText"></span></p>
+        <div x-cloak x-show="inputedText.length > 0 && inputedText.length < 4">
+            <p>4文字以上入れてね</p>
+        </div>
+        <div x-cloak x-show="inputedText == 'test'">
+            <div class="text-red-300">testって入れてしまいましたね</div>
+        </div>
+    </div>
+    <br>
+
+    <!-- プルダウンメニュー -->
+    <div x-data="{ dropdown: false }" class="p-2">
+        <button @click="dropdown = true" class="px-4 py-2 text-white bg-blue-500 rounded-md">プルダウンメニュー</button>
+        <ul @click.away="dropdown = false" x-transition.opacity.duration.500ms x-cloak x-show="dropdown" class="absolute w-40 border px-1 py-2 bg-gray-50 rounded-md">
+            <li class="p-2"><a href="#" class="block p-2 hover:bg-gray-300">プルダウン？</a></li>
+            <li class="p-2"><a href="#" class="block p-2 hover:bg-gray-300">ドロップダウン？</a></li>
+            <li class="p-2"><a href="#" class="block p-2 hover:bg-gray-300">どちらでもいいか</a></li>
+        </li>
+    </div>
+    <br>
+
+    <!-- 候補検索 -->
+    <div
+        x-data="{
+            search: '',
+            items: ['あめんぼ', 'あかいな', 'あいうえお', 'あいうえを'],
+            get filteredItems() {
+                return this.items.filter(
+                    i => i.startsWith(this.search)
+                )
+            }
+        }"
+    >
+        <input x-model="search" placeholder="候補検索">
+        <ul>
+            <template x-for="item in filteredItems" :key="item">
+                <li x-text="item"></li>
+            </template>
+        </ul>
+    </div>
+    <br>
+
+    <!-- for文で要素の抜き出し template 内しかできない -->
+    <ul x-data="{ words: ['あああ', 'いいい', 'ううう'] }">
+        <template x-for="(word, index) in words">
+            <li>
+                <span x-text="index + ': '"></span>
+                <span x-text="word"></span>
+            </li>
+        </template>
+    </ul>
+
+    <!-- モーダルを他の場所クリックで閉じる -->
+    <div x-data="{open: false}">
+        <div>
+            <button x-on:click="open = true" class="border p-3 bg-gray-200 rounded-xl border-black shadow-lg m-4">説明しよう</button>
+        </div>
+
+        <div x-cloak x-show="open" class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40 flex justify-center items-center border">
+            <div x-on:click.away="open = false" class="max-w-xs w-full h-auto flex flex-col bg-white rounded-xl px-4 py-2">
+                <!-- モーダル本体に x-on:click.away="open = false" -->
+                <header class="flex justify-between items-end pb-2 mb-2 border-b border-black">
+                    <p>説明しよう</p>
+                    <div x-on:click="open = false" class="text-2xl cursor-pointer">×</div>
+                </header>
+                <div class="py-2 px-1">
+                    モーダルは×ボタンでしか閉じられないとイラつくのだ。<br>
+                    だから、他の場所をクリックしても閉じられるようにしたい。
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- 表示されるときの速さとか -->
     <div x-data="{
         open: false,
         toggle() {
@@ -10,12 +177,12 @@
     }">
         <button @click="toggle" class="font-display p-2 m-5 bg-green-300 rounded">表示</button>
         <!-- 。関数 toggle を呼んでいる -->
-        <div x-show="open" x-transition.opacity.scale.10.origin.top.duration.1000ms >
+        <div x-cloak x-show="open" x-transition.opacity.scale.10.origin.top.duration.1000ms >
             やっほー
         </div>
 
         <div class="flex px-4 py-2 w-auto">
-            <ul x-show="open" x-transition.scale.10.origin.left.duration.1000ms >
+            <ul x-cloak x-show="open" x-transition.scale.10.origin.left.duration.1000ms >
                 <li>1行目</li>
                 <li>2行目</li>
                 <li>3行目</li>
@@ -28,7 +195,7 @@
     <!-- x-data の中身が大きくなる場合は、別で定義する -->
     <div x-data="toggleText()">
         <button @click="toggle" class="font-display p-2 m-5 bg-yellow-300 rounded">toggleText</button>
-        <div x-show="open">
+        <div x-cloak x-show="open">
             toggleTextやっほー
         </div>
     </div>
@@ -56,12 +223,12 @@
 
     <div class="flex justify-around items-center w-full" x-data="{ toggle : false}">
         <button @click=" toggle = !toggle " x-text="toggle ? 'hide' : 'show'" class="font-display p-2 m-5 bg-blue-300 rounded"></button>
-        <h1 x-show="toggle" class="font-display">alpine</h1>
+        <h1 x-cloak x-show="toggle" class="font-display">alpine</h1>
     </div>
 
     <div class="flex items-center w-full" x-data="{toggle: false}">
         <button @click="toggle = ! toggle" class="py-2 px-3 mx-10 rounded-sm bg-blue-300">Toggle</button>
-        <div x-show="toggle"
+        <div x-cloak x-show="toggle"
             x-transition:enter="transition ease-out duration-300"
             x-transition:enter-start="opacity-0 scale-90"
             x-transition:enter-end="opacity-100 scale-100"
@@ -74,7 +241,7 @@
     <br>
     <div class="flex items-center" x-data="{isShow : false}">
         <button @click="isShow = !isShow" x-text="isShow ? '非表示':'表示'" class="font-display p-2 m-5 bg-blue-300 rounded"></button>
-        <div x-show="isShow">
+        <div x-cloak x-show="isShow">
                 Hello alpine!
         </div>
         <button @click="isShow = !isShow" x-text="isShow ? '非表示':'表示'" class="font-display p-2 m-5 bg-blue-300 rounded"></button>
@@ -83,7 +250,7 @@
 
     <div class="relative" x-data="{ isOpen:false }" @click.away="isOpen = false">
         <button class="text-black shadow-lg rounded-md" @click="isOpen = !isOpen">ドロップダウンメニュー</button>
-        <div class="bg-white absolute w-56 my-2 py-1 shadow-lg rounded-md" x-show="isOpen">
+        <div class="bg-white absolute w-56 my-2 py-1 shadow-lg rounded-md" x-cloak x-show="isOpen">
             <a href="#" class="block p-2 hover:bg-gray-100">alpine1</a>
             <a href="#" class="block p-2 hover:bg-gray-100">alpine2</a>
             <a href="#" class="block p-2 hover:bg-gray-100">alpine3</a>
@@ -126,6 +293,7 @@
         </x-dropdown>  
     </div>
 
+    <!-- tailwind で自分なりのCSSクラス -->
     <!-- app.css に定義 -->
     <div class="mt-5">
         <input type="text" class="form-control-test" placeholder="テキストフィールド">
