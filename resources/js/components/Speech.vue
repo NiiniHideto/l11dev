e91cda08439af70e1b31b8157fe2694f89bb7643
niimi <hideto.niimi@1plus.co.jp>
2024-11-26 17:18:15 +0900

    ソースの整理

--- resources/js/components/Speech.vue
+++ resources/js/components/Speech.vue
@@ -6,15 +6,7 @@
         await createAudio(message.value);
     };
 
-    // const chatGptClick = async () => {
-    //     const responseText = await requestChatAPI(chatgpt.value);
-    //     await createAudio(responseText);
-    //     const output = document.querySelector(".output");
-    //     output.textContent = responseText;
-    // };
-
-    const message = ref('initial');
-    const chatgpt = ref('chatGPT initial');
+    const message = ref('');
 
     async function createAudio(text) {
         const audio = document.querySelector(".audio");
@@ -24,10 +16,7 @@
 
     async function getAudioURL(text) {
         const query = await getQuery(text);
-
-        console.log(query);
-
-
+        // console.log(query);
         const response = await axios.post(
             "http://localhost:10101/synthesis?speaker=888753760",
             query,
@@ -43,38 +32,6 @@
         return response.data;
     }
 
-    const api_key = "各自のキー";
-
-    async function requestChatAPI(text) {
-        const headers = {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${api_key}`,
-        };
-
-        const messages = [
-            {
-                role: "user",
-                content: text,
-            },
-        ];
-
-        const payload = {
-            model: "gpt-3.5-turbo",
-            max_tokens: 128,
-            messages: messages,
-        };
-
-        const response = await axios.post(
-            "https://api.openai.com/v1/chat/completions",
-            payload,
-            {
-                headers: headers,
-            }
-        );
-        console.log('ChatGPT送信');
-        return response.data.choices[0].message.content;
-    }
-
     // ※ firefox は対応していない
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     // ブラウザが API をサポートしているかどうかを確認します
@@ -87,44 +44,17 @@
         recognition.lang = language;
         recognition.interimResults = true;
         recognition.continuous = true;
-        recognition.onresult = handleResult;
     }
-
-    async function handleResult(event) {
-        let finalTranscript = '';
-        
-        for (let i = event.resultIndex; i < event.results.length; i++) {
-            const transcript = event.results[i][0].transcript;
-            console.log(transcript);
-
-            if (event.results[i].isFinal) {
-                finalTranscript += transcript;
-                const responseText = await requestChatAPI(finalTranscript);
-                await createAudio(responseText);
-                const output = document.querySelector(".output");
-                output.textContent = responseText;
-            }
-        }
-    }
-
-    const start = () => {
-        recognition.start();
-    };
-    const stop = () => {
-        recognition.stop();
-    };
-
 </script>
 
 
 <template>
-
     <div v-if="isSupported">
         <span>AivisSpeech:</span>
         <br>
         <p style="white-space: pre-line">{{ message }}</p>
         <textarea style="color:black" v-model="message" placeholder="add multiple lines"></textarea>
-
+        <br>
         <button @click="buttonClicked">
             送信
         </button>
@@ -138,88 +68,10 @@
     <div v-if="!isSupported">
         <span>Your browser does not support SpeechRecognition.</span>
     </div>
-
-
-
-    <!-- <v-row>
-      <v-col cols="4">
-        <v-card
-            class="mx-auto"
-            max-width="344"
-            variant="outlined"
-          >
-          <v-card-item>
-            <div class="text-overline mb-1">
-              入力したものをずんだもんに話してもらう
-              <v-text-field v-model="val"></v-text-field>
-            </div>
-          </v-card-item>
-  
-          <v-card-actions>
-            <v-btn variant="outlined" v-on:click="buttonClicked">
-              送信
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-        <audio class="audio"></audio>
-      </v-col>
-  
-      <v-col cols="4">
-        <v-card
-            class="mx-auto"
-            max-width="344"
-            variant="outlined"
-          >
-          <v-card-item>
-            <div class="text-overline mb-1">
-              入力したものをChatGPTに飛ばす
-              <v-text-field v-model="chatgpt"></v-text-field>
-            </div>
-          </v-card-item>
-  
-          <v-card-actions>
-            <v-btn variant="outlined" v-on:click="chatGptClick">
-              ChatGPTへ送信
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-        <audio class="audio"></audio>
-      </v-col>
-  
-      <v-col cols="4">
-        <v-card
-            class="mx-auto"
-            max-width="344"
-            variant="outlined"
-          >
-          <v-card-item>
-              ずんだもんに話しかける
-          </v-card-item>
-  
-          <v-card-actions>
-            <v-btn variant="outlined" v-on:click="start">
-              開始
-            </v-btn>
-            <v-btn variant="outlined" v-on:click="end">
-              終了
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-        <audio class="audio"></audio>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <div class="output"></div>
-      </v-col>
-    </v-row> -->
-
-
-  </template>
+</template>
 
 
 <style scoped>
-
     button {
         font-weight: bold;
         background-color: #04AA6D;
@@ -240,5 +92,4 @@
         color: black;
         box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
     }
-
 </style>
